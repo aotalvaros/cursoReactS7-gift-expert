a2959b1704f2fd9a6fe28451c4d3e78f96af1be4
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-09-09 14:36:42 -0500

    Se le da un formato al GifExpertApp

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,33 +1,26 @@
-import React,{ useState } from 'react';
-import { AddCategory } from './components/AddCategory';
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
-export const GifExpertApp = ()=>{
+export const GifExpertApp = () => {
+  
+    const [categories, setcategories] = useState([
+    "Mad Max",
+  ]);
 
-    //creamos una lista de categorias 
-    //const categories =['Mad Max','Jhon Wick','Dark'];
+  //Se crea otro componente que recibira categories como argumento "GifGrid.js"
 
-    const [categories, setcategories] = useState(
-        ['Mad Max','Jhon Wick','Dark']);
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setcategories={setcategories} />
+      <hr />
 
-        //Se utilizo en un boton
-    // const handleAdd=()=>{
-    //     setcategories(cats=>[...cats,'Mortal Kombat'])
-    // }
-
-    return(
-        <>
-            <h2>GifExpertApp</h2>
-            <AddCategory setcategories={setcategories}/>
-            <hr />
-   
-            <ol>
-                {/* Se utilizara una expresion que recorra cada uno de los elementos del arreglo*/}
-                {
-                    categories.map(category=>{
-                        return <li key={ category }> { category }</li>
-                    })
-                }
-            </ol>
-        </>
-    )
-}
+      <ol>
+        {/* Se utilizara una expresion que recorra cada uno de los elementos del arreglo*/}
+        {categories.map((category) => {
+          return <li key={category}> {category}</li>;
+        })}
+      </ol>
+    </>
+  );
+};
