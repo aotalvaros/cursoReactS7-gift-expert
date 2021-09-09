77f91d91b8e70f7a7b8fcb0723012dc597acbda0
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-09-09 10:18:53 -0500

    Se crea una lista de categorias y lo mostramos en un lista del DOM

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = ()=>{
+
+    //creamos una lista de categorias 
+    //const categories =['Mad Max','Jhon Wick','Dark'];
+
+    const [categories, setcategories] = useState(
+        ['Mad Max','Jhon Wick','Dark']);
+
+        //Se utilizo en un boton
+    // const handleAdd=()=>{
+    //     setcategories(cats=>[...cats,'Mortal Kombat'])
+    // }
 
     return(
         <>
             <h2>GifExpertApp</h2>
+            <AddCategory/>
             <hr />
+   
+            <ol>
+                {/* Se utilizara una expresion que recorra cada uno de los elementos del arreglo*/}
+                {
+                    categories.map(category=>{
+                        return <li key={ category }> { category }</li>
+                    })
+                }
+            </ol>
         </>
     )
 }
