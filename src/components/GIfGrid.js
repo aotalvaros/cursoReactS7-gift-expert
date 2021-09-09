c7b6eaedb74897fc03a7338165eb55d21ea8fb6e
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-09-09 16:34:46 -0500

    Se cambia la peticion del HTTP a otro archivo

--- src/components/GIfGrid.js
+++ src/components/GIfGrid.js
@@ -1,32 +1,17 @@
 import React,{ useState, useEffect } from 'react' //useEffect: me permite ejecutar sierto codigo de una manera condicional
+import { getGifs } from '../helpers/getGifs';
 import { GIfGridItem } from './GIfGridItem';
 
-export const GIfGrid = ({category}) => {
+export const GIfGrid = ({ category }) => {
     // se recibe la categoria(category) y se va hacer peticion HTTP
 
     const [images, setimages] = useState([]);
 
     useEffect(()=>{ // se ejecuta esta instruccion cuando el componente es renderizado por primera vez
-        getGifs();
-    },[])
+        getGifs(category)
+            .then( setimages );
+    },[ category  ])
 
-    const getGifs = async()=>{
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=Mortal+Kombat+pelicula&limit=5&api_key=4YUwstEQP72nAiRk1akZMJhHJp5H2Pme';
-        const resp = await fetch( url );
-        const {data} = await resp.json();
-
-        const gifs = data.map( img => {
-            return{
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        } )
-
-        console.log(gifs);
-        setimages(gifs);
-    }
 
     return (
         <>
