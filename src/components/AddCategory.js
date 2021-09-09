fb1349d4eac18444a8b444e9a71f04a66aba41c3
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-09-09 14:30:43 -0500

    Se crea un archivo para manejar un componente input que recibira un valor requerido y manejara un evento al darle enter en el input

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
+import  PropTypes  from 'prop-types';
 
-export const AddCategory = () => {
+export const AddCategory = ({setcategories}) => {
+
+    //para saber lo que la persona esta escribiendo se ncesita tener un estado en el input
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (e)=>{
+        setInputValue(e.target.value);
+    }
+
+    //manejamos un enter en el input
+    const handleSubmit= (e)=>{
+        e.preventDefault();
+    
+        //una validacion para que no sea vacio
+        if(inputValue.trim().length>2){
+            setcategories(cats=>[...cats,inputValue]);
+            setInputValue('');
+        }
+
+        
+    }
+
     return (
-        <>
-            <h2>Add Category</h2>
-        </>
+        <form onSubmit={ handleSubmit }>
+            <input 
+                type='text'
+                value={ inputValue }
+                onChange={handleInputChange}
+            />
+        </form>
     )
 }
+
+AddCategory.prototypes={
+    setcategories: PropTypes.func.isRequired
+}
+
