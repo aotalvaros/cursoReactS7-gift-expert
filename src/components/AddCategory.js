b325b8067e8183593f27fb074e2f65cd285982bc
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-09-09 14:36:20 -0500

    Se le da un formato al archivo AddCategory.js

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,40 +1,35 @@
-import React,{useState} from 'react'
-import  PropTypes  from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-export const AddCategory = ({setcategories}) => {
-
-    //para saber lo que la persona esta escribiendo se ncesita tener un estado en el input
-    const [inputValue, setInputValue] = useState('');
-
-    const handleInputChange = (e)=>{
-        setInputValue(e.target.value);
-    }
-
-    //manejamos un enter en el input
-    const handleSubmit= (e)=>{
-        e.preventDefault();
+export const AddCategory = ({ setcategories }) => {
     
-        //una validacion para que no sea vacio
-        if(inputValue.trim().length>2){
-            setcategories(cats=>[...cats,inputValue]);
-            setInputValue('');
-        }
+  //para saber lo que la persona esta escribiendo se ncesita tener un estado en el input
+  const [inputValue, setInputValue] = useState("");
 
-        
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  //manejamos un enter en el input
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //una validacion para que no sea vacio
+    if (inputValue.trim().length > 2) {
+      setcategories((cats) => [...cats, inputValue]);
+      setInputValue("");
     }
 
-    return (
-        <form onSubmit={ handleSubmit }>
-            <input 
-                type='text'
-                value={ inputValue }
-                onChange={handleInputChange}
-            />
-        </form>
-    )
-}
+    //validamos que cuando halla un nuevo componente en la lista haga la peticion HTTP
+  };
 
-AddCategory.prototypes={
-    setcategories: PropTypes.func.isRequired
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+    </form>
+  );
+};
 
+AddCategory.prototypes = {
+  setcategories: PropTypes.func.isRequired,
+};
