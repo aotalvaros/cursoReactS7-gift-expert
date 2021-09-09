cb636baa5ed9f9493d7c2df3731b107d59c43a0a
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-09-09 15:02:24 -0500

    Se utiliza el fetch para hacer la peticion de l http para mostrar un gif

--- src/components/GIfGrid.js
+++ src/components/GIfGrid.js
@@ -1,6 +1,27 @@
 import React from 'react'
 
 export const GIfGrid = ({category}) => {
+    // se recibe la categoria(category) y se va hacer peticion HTTP
+
+    const getGifs = async()=>{
+
+        const url = 'https://api.giphy.com/v1/gifs/search?q=Mortal+Kombat+pelicula&limit=5&api_key=4YUwstEQP72nAiRk1akZMJhHJp5H2Pme';
+        const resp = await fetch( url );
+        const {data} = await resp.json();
+
+        const gifs = data.map( img => {
+            return{
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url
+            }
+        } )
+
+        console.log(gifs);
+    }
+
+    getGifs();
+
     return (
         <div>
             <h3>{ category }</h3>
