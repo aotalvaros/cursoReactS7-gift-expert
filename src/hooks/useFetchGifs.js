2bdfc3423e4c2ff53f848108b60ae78ef4d46277
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-09-10 09:12:06 -0500

    Se le da un formato y un comentario al archivo useFetchGifs

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,27 +1,22 @@
-import { useState,useEffect } from "react";//useEffect: me permite ejecutar sierto codigo de una manera condicional
+import { useState, useEffect } from "react"; //useEffect: me permite ejecutar sierto codigo de una manera condicional
 import { getGifs } from "../helpers/getGifs";
 
-export const useFetchGifs = ( category ) =>{
+export const useFetchGifs = (category) => {
+  const [state, setstate] = useState({
+    data: [],
+    loading: true,
+  });
 
-    const [state, setstate] = useState({
-        
-        data: [],
-        loading: true
+  useEffect(() => {// se ejecuta esta instruccion cuando el componente es renderizado por primera vez
+    getGifs(category).then((imgs) => {
+      setTimeout(() => {
+        setstate({
+          data: imgs,
+          loading: false,
+        });
+      }, 3000);
     });
+  }, [category]);
 
-    useEffect(() => {
-        getGifs( category )
-            .then( imgs =>{
-
-                setTimeout(() => {
-                    setstate({
-                        data: imgs,
-                        loading: false
-                    });
-                }, 3000);
-            })
-
-    }, [category])
-
-    return state;
-} 
+  return state;
+};
